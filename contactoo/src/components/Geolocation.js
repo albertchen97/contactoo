e689c6c4903dc2bac4e2414a435f55b455d4a219
Albert (Haoze Chen) <albertchen2017@outlook.com>
2022-08-24 18:06:43 -0700

    update geolocation

--- contactoo/src/components/Geolocation.js
+++ contactoo/src/components/Geolocation.js
@@ -2,21 +2,21 @@
 // YouTube Tutorial: https://www.youtube.com/watch?v=U3dLjHN0UvM&ab_channel=TGTech
 // HTML Geolocation: https://www.w3schools.com/html/html5_geolocation.asp
 import React from "react";
-var x = document.getElementById("demo");
-function showPosition(position) {
-  x.innerHTML =
-    "Latitude: " +
-    position.coords.latitude +
-    "<br>Longitude: " +
-    position.coords.longitude;
-}
+// var x = document.getElementById("demo");
+// function showPosition(position) {
+//   x.innerHTML =
+//     "Latitude: " +
+//     position.coords.latitude +
+//     "<br>Longitude: " +
+//     position.coords.longitude;
+// }
 
 class Geolocation extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       latitude: null,
-      longtitude: null,
+      longitude: null,
       userAddress: null,
     };
     this.getLocation = this.getLocation.bind(this);
@@ -24,14 +24,39 @@ class Geolocation extends React.Component {
   }
   getLocation() {
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(showPosition);
+      navigator.geolocation.getCurrentPosition(
+        this.getCoordinates,
+        this.handleLocationError
+      );
     } else {
       alert("Geolocation is not supported by this browser!");
     }
   }
   getCoordinates(position) {
     console.log(position.coords.latitude);
-    // this.setState({})
+    console.log(position.coords.longitude);
+    this.setState({
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude,
+    });
+  }
+  handleLocationError(error) {
+    switch (error.code) {
+      case error.PERMISSION_DENIED:
+        alert(
+          "You denied the request for Geolocation. Please enable it in your browser setting to use the live location feature."
+        );
+        break;
+      case error.POSITION_UNAVAILABLE:
+        alert("Location information is unavailable.");
+        break;
+      case error.TIMEOUT:
+        alert("The request to get user location timed out.");
+        break;
+      case error.UNKNOWN_ERROR:
+        alert("An unknown error occurred.");
+        break;
+    }
   }
 
   render() {
@@ -41,9 +66,14 @@ class Geolocation extends React.Component {
         <button onClick={this.getLocation}>Get Coordinates</button>
         <h4>HTML5 Coordinates</h4>
         <p>Latitude: {this.state.latitude}</p>
-        <p>Longtitude: {this.state.longtitude}</p>
+        <p>Longtitude: {this.state.longitude}</p>
         <h4>Google Maps Reverse Geocoding</h4>
         <p>Address: {this.state.userAddress}</p>
+        {
+          // render the map if the geolocation is available; else do nothing
+          this.state.latitude && this.state.longitude ?
+            <img src={ } alt='' /> : null
+        }
       </div>
     );
   }
