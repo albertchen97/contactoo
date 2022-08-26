24773ddcfde7ed1dd1c039200e7e6cd6cb4a7739
Albert (Haoze Chen) <albertchen2017@outlook.com>
2022-08-26 13:47:46 -0700

    add chat page

---
+++ live-chat/pages/chat.js
@@ -0,0 +1,164 @@
+import React, { useEffect, useState } from "react";
+
+import styles from "../styles/Home.module.css";
+// The "withAuthenticator" is a higher-order component (HoC) that wraps "AmplifyAuthenticator" so that the browser will only render the home page if the user is signed in.
+import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
+import { API, Auth, withSSRContext, graphqlOperation } from "aws-amplify";
+import { listMessages } from "../src/graphql/queries";
+import { createMessage } from "../src/graphql/mutations";
+import Message from "../components/message";
+import { onCreateMessage } from "../src/graphql/subscriptions";
+
+// Render the chat app on the Home page
+// Chat app is a container with a input form and a submit button
+function Chat({ messages }) {
+  const [stateMessages, setStateMessages] = useState([...messages]);
+  const [messageText, setMessageText] = useState("");
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const amplifyUser = await Auth.currentAuthenticatedUser();
+        setUser(amplifyUser);
+      } catch (err) {
+        setUser(null);
+      }
+    };
+
+    fetchUser();
+
+    // Subscribe to creation of message
+    const subscription = API.graphql(
+      graphqlOperation(onCreateMessage)
+    ).subscribe({
+      next: ({ provider, value }) => {
+        setStateMessages((stateMessages) => [
+          ...stateMessages,
+          value.data.onCreateMessage,
+        ]);
+      },
+      error: (error) => console.warn(error),
+    });
+  }, []);
+
+  useEffect(() => {
+    async function getMessages() {
+      try {
+        const messagesReq = await API.graphql({
+          query: listMessages,
+          authMode: "AMAZON_COGNITO_USER_POOLS",
+        });
+        setStateMessages([...messagesReq.data.listMessages.items]);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    getMessages();
+  }, [user]);
+
+  const handleSubmit = async (event) => {
+    // Prevent the page from reloading
+    event.preventDefault();
+
+    // clear the textbox
+    setMessageText("");
+
+    const input = {
+      // id is auto populated by AWS Amplify
+      message: messageText, // the message content the user submitted (from state)
+      owner: user.username, // this is the username of the current user
+    };
+
+    // Try make the mutation to graphql API
+    try {
+      await API.graphql({
+        authMode: "AMAZON_COGNITO_USER_POOLS",
+        query: createMessage,
+        variables: {
+          input: input,
+        },
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  if (user) {
+    return (
+      <div className={styles.background}>
+        <div className={styles.container}>
+          {/* <AmplifySignOut /> */}
+          <h1 className={styles.title}> AWS Amplify Live Chat</h1>
+          <div className={styles.chatbox}>
+            {stateMessages
+              // sort messages oldest to newest client-side
+              .sort((a, b) => b.createdAt.localeCompare(a.createdAt))
+              .map((message) => (
+                // map each message into the message component with message as props
+                <Message
+                  message={message}
+                  user={user}
+                  isMe={user.username === message.owner}
+                  key={message.id}
+                />
+              ))}
+          </div>
+          <div className={styles.formContainer}>
+            <form onSubmit={handleSubmit} className={styles.formBase}>
+              <input
+                type="text"
+                id="message"
+                name="message"
+                autoFocus
+                required
+                value={messageText}
+                onChange={(e) => setMessageText(e.target.value)}
+                placeholder="💬 Send a message to the world 🌎"
+                className={styles.textBox}
+              />
+              <button style={{ marginLeft: "8px" }}>Send</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    return <p>Loading...</p>;
+  }
+}
+
+export default withAuthenticator(Chat);
+
+export async function getServerSideProps({ req }) {
+  // SSR stands for "Server-Side Rendering."
+  // wrap the request in a withSSRContext to use Amplify functionality serverside.
+  const SSR = withSSRContext({ req });
+
+  try {
+    // currentAuthenticatedUser() will throw an error if the user is not signed in.
+    const user = await SSR.Auth.currentAuthenticatedUser();
+
+    // If we make it passed the above line, that means the user is signed in.
+    const response = await SSR.API.graphql({
+      query: listMessages,
+      // use authMode: AMAZON_COGNITO_USER_POOLS to make a request on the current user's behalf
+      authMode: "AMAZON_COGNITO_USER_POOLS",
+    });
+
+    // return all the messages from the dynamoDB
+    return {
+      props: {
+        messages: response.data.listMessages.items,
+      },
+    };
+  } catch (error) {
+    // We will end up here if there is no user signed in.
+    // We'll just return a list of empty messages.
+    return {
+      props: {
+        messages: [],
+      },
+    };
+  }
+}
--- live-chat/pages/index.js
+++ live-chat/pages/index.js
@@ -1,7 +1,7 @@
+import React, { useEffect, useState } from "react";
 import Head from "next/head";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  return <div></div>;
+  return <div>Hello</div>;
 }
