fa249b528500561e0e1c4aa5be14e0748581e8dd
Albert (Haoze Chen) <albertchen2017@outlook.com>
2022-08-26 13:56:17 -0700

    delete old project

--- live-chat/pages/index.js
+++
@@ -1,7 +0,0 @@
-import React, { useEffect, useState } from "react";
-import Head from "next/head";
-import Image from "next/image";
-
-export default function Home() {
-  return <div>Hello</div>;
-}
