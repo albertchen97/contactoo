f1d146d532c565f154e64d1b2ce3247589fd35dc
YuetHo <yhe944@gmail.com>
2022-08-29 17:00:02 -0400

    New home page UI built in TailWind

---
+++ contactoo/public/imageIndex.js
@@ -0,0 +1,14 @@
+import mainLogo from './mainLogo.svg'
+import splashBG from './splashBG.png'
+import magnifyGlass from './magnifyGlass.svg'
+import smsLogo from './smsLogo.svg'
+import emailLogo from './emailLogo.svg'
+
+
+export {
+  mainLogo,
+  splashBG,
+  magnifyGlass,
+  smsLogo,
+  emailLogo
+}
