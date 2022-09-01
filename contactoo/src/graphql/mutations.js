cd874094b6e5ba0fbeaa3b71c7a971c1e10a448b
mohammadnayeem1 <mohammadnayeem2000@gmail.com>
2022-08-31 21:12:54 -0400

    graphql api

--- contactoo/src/graphql/mutations.js
+++ contactoo/src/graphql/mutations.js
@@ -1,6 +1,66 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
+export const createSession = /* GraphQL */ `
+  mutation CreateSession(
+    $input: CreateSessionInput!
+    $condition: ModelSessionConditionInput
+  ) {
+    createSession(input: $input, condition: $condition) {
+      id
+      status
+      createdAt
+      messages {
+        id
+        owner
+        message
+        createdAt
+        updatedAt
+      }
+      updatedAt
+    }
+  }
+`;
+export const updateSession = /* GraphQL */ `
+  mutation UpdateSession(
+    $input: UpdateSessionInput!
+    $condition: ModelSessionConditionInput
+  ) {
+    updateSession(input: $input, condition: $condition) {
+      id
+      status
+      createdAt
+      messages {
+        id
+        owner
+        message
+        createdAt
+        updatedAt
+      }
+      updatedAt
+    }
+  }
+`;
+export const deleteSession = /* GraphQL */ `
+  mutation DeleteSession(
+    $input: DeleteSessionInput!
+    $condition: ModelSessionConditionInput
+  ) {
+    deleteSession(input: $input, condition: $condition) {
+      id
+      status
+      createdAt
+      messages {
+        id
+        owner
+        message
+        createdAt
+        updatedAt
+      }
+      updatedAt
+    }
+  }
+`;
 export const createMessage = /* GraphQL */ `
   mutation CreateMessage(
     $input: CreateMessageInput!
