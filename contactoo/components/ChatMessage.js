1f1a907ad7a50731170d5cee381fde471d6f39c1
Albert (Haoze Chen) <albertchen2017@outlook.com>
2022-09-01 11:52:28 -0700

    Fixed the 'duplicate key' error by using uuidv4. Moved chat.js to components/ folder.

--- contactoo/components/ChatMessage.js
+++ contactoo/components/ChatMessage.js
@@ -1,9 +1,15 @@
 import React from "react";
 import styles from "../styles/ChatMessage.module.css";
+// Use uuid to solve the duplicate messages bug
+import { v4 as uuidv4 } from "uuid";
 
 export default function ChatMessage({ message, isMe }) {
+  message.id = uuidv4();
   return (
-    <div className={isMe ? styles.sentMessageContainer : styles.receivedMessageContainer}>
+    <div
+      className={
+        isMe ? styles.sentMessageContainer : styles.receivedMessageContainer
+      }>
       <p className={styles.senderText}>{message.owner}</p>
       <div className={isMe ? styles.sentMessage : styles.receivedMessage}>
         <p>{message.message}</p>
