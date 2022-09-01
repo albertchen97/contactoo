cf9e57ae52186633bf8efaf9c62b1d63fa442787
matthewblu421 <44147556+matthewblu421@users.noreply.github.com>
2022-09-01 01:51:09 -0400

    SMS function working in nextJS version of project

--- contactoo/next.config.js
+++ contactoo/next.config.js
@@ -2,6 +2,21 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+(module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://api.example.com/:path*",
+      },
+    ];
+  },
+}),
+  nextConfig,
+  {
+    env: {
+      customKey: "my-value",
+    },
+  };
