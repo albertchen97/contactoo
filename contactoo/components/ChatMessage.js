a8c454e74ff19939f57f1bb6f3e470fc0bbee969
Albert (Haoze Chen) <albertchen2017@outlook.com>
2022-09-05 09:57:35 -0700

    Clean up the code.

--- contactoo/components/ChatMessage.js
+++ contactoo/components/ChatMessage.js
@@ -1,40 +1,6 @@
-import React, { useEffect, useState } from "react";
 import styles from "../styles/ChatMessage.module.css";
-// Use uuid to solve the duplicate messages bug
-import { v4 as uuidv4 } from "uuid";
-import Chat from "./Chat";
 
 export default function ChatMessage({ message, isMe }) {
-  console.log("In ChatMessage():");
-  console.log("message.message = ", message.message);
-  // console.log("isMe = ", isMe);
-
-  // // const [messageId, setMessageId] = useState(uuidv4());
-  // const [messageId, setMessageId] = useState(() => {
-  //   console.log("messageId: ", messageId);
-  //   return uuidv4();
-  //   // return null;
-  // });
-  // useEffect(() => {
-  //   setMessageId(uuidv4());
-  //   console.log("messageId: ", messageId);
-  // }, [message]);
-
-  // Chat(messageId);
-  // useEffect(() => {
-  //   messageId = uuidv4();
-  // });
-
-  // function handleChatMessage() {
-  //   console.log("message updated, generating new messageId");
-
-  //   // // generate a uuid for the messageId
-  //   // setMessageId(uuidv4());
-
-  //   console.log("messageId for ", message, ": ", messageId);
-  // }
-
-  // console.log("In ChatMessage(): message: ", message);
   return (
     <div
       className={
@@ -44,7 +10,6 @@ export default function ChatMessage({ message, isMe }) {
       <div className={isMe ? styles.sentMessage : styles.receivedMessage}>
         <p>{message.message}</p>
       </div>
-      {/* <Chat messageId={messageId} /> */}
     </div>
   );
 }
