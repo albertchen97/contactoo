6cd53cce3696429bd534c5e5d5a2b0d08b63a131
YuetHo <yhe944@gmail.com>
2022-09-10 20:09:12 -0400

    properly aligned admin ui

--- contactoo/components/CustomerList.js
+++ contactoo/components/CustomerList.js
@@ -5,7 +5,7 @@ export default function CustomerList({ user }) {
   // TODO: add functions to read customers from DB and render them as a CustomerTicket into the list
 
   return (
-    <ul className="w-full h-full divide-y divide-gray-500 ">
+    <ul className="w-full divide-y divide-gray-500 ">
       <CustomerTicket name="Tom" email="tom@gmail.com" />
       <CustomerTicket name="Jone" email="Jone@gmail.com" />
       <CustomerTicket name="Reallylongname" email="Reallylongname@gmail.com" />
