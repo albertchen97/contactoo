4ed293ed82048a01860ac1f3f6913df399474191
Haoze Chen <albertchen2017@outlook.com>
2023-05-08 05:41:04 -0700

    comment out aws imports for testing

--- contactoo/pages/_app.js
+++ contactoo/pages/_app.js
@@ -1,8 +1,8 @@
 import 'react-toastify/dist/ReactToastify.css';
 import "../styles/globals.css";
-import Amplify from "aws-amplify";
-import awsconfig from "../src/aws-exports";
-Amplify.configure({ ...awsconfig, ssr: true });
+// import Amplify from "aws-amplify";
+// import awsconfig from "../src/aws-exports";
+// Amplify.configure({ ...awsconfig, ssr: true });
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
